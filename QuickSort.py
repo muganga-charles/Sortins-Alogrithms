bbef71f5177b117aede2136e5b958ac1c484c366
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-26 21:18:36 +0300

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,8 +1,8 @@
 def partition (array,low,high):
-    pivort = array[high]
+    pivot = array[high]
     i = low - 1
     for j in range(low,high):
-        if array[j] <= pivort:
+        if array[j] <= pivot:
             i = i+ 1
             (array[i],array[j]) = (array[j],array[i])
     (array[i+1],array[high]) = (array[high],array[i+1])
