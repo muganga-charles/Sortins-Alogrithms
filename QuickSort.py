d012a05e03deba5d57d75ba78d94b0af07478b28
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-10-26 21:01:21 +0300

    Add files via upload

---
+++ QuickSort.py
@@ -0,0 +1,23 @@
+def partition (array,low,high):
+    pivort = array[high]
+    i = low - 1
+    for j in range(low,high):
+        if array[j] <= pivort:
+            i = i+ 1
+            (array[i],array[j]) = (array[j],array[i])
+    (array[i+1],array[high]) = (array[high],array[i+1])
+    return i+1
+
+def QuickSort(array,low,high):
+    if low < high:
+        pi = partition(array,low,high)
+        QuickSort(array,low,pi-1)
+        QuickSort(array,pi+1,high)
+        
+def main():
+    array = [1,7,4,1,10,9,-2]
+    size =  len(array)
+    QuickSort(array,0,(size-1))
+    print(array)
+
+main()
